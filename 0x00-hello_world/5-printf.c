347e5c81c05ea0404cc93b8bef20124ac6dbc35b
nixonomunga <nixonomunga@gmail.com>
2023-08-10 03:21:36 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/**
+ *Program name: 5-printf.c
+ *
+ *Made by: Nixon
+ */
+
+#include <stdio.h>
+/**
+ *main : is void and prints inserted words
+ *
+ *Return : 0 Always.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
