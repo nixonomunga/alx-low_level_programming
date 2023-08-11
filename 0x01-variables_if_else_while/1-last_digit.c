f94fa0fb8f29283ea7c8d65063a2280fb90965df
nixonomunga <nixonomunga@gmail.com>
2023-08-11 08:50:18 -0700

    combined

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,39 +4,36 @@
  * Made by: Nixon
  */
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
-/**
- * main - Entry point
+/* main - entry point
  *
- * Description: Prints last digit with description
+ * Description: Prints last digit
  *
  * Return: 0 always
  */
 
 int main(void)
 {
-	int n, last_d;
+  int n, last_d;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_d = n % 10;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  last_d = n % 10;
 
-	if (n > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,
-		       last_d);
-	}
-	else if (n == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, last_d);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-		       n, last_d);
-	}
+  if (last_d > 5)
+    {
+      printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+    }
+  else if (last_d == 0)
+    {
+      printf("Last digit of %d is %d and is 0\n", n, last_d);
+    }
+  else if (last_d < 6 && last_d != 0)
+    {
+      printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+    }
+  return (0);
 
-	return (0);
 }
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_tebahpla.c
+ *
+ * Made by: Nixon
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ *
+ * Description: Alphabet in reverse
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+  char u = 'z';
+  for (; u >= 'a'; u--)
+    {
+      putchar(u);
+    }
+
+  putchar(\n);
+
+  return (0);
+}
