9ddfd2d30891e8af688ebe6ec2accc7b16412635
nixonomunga <nixonomunga@gmail.com>
2023-08-11 04:00:55 -0700

    Positive or Negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,39 @@
+/*
+ * File name: 0-positive_or_negative.c
+ *
+ * Made by: Nixon
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: asigns random number to a variable n
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is poitive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
