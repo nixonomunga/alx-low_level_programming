f591f3a95c2c5a94908e7028ded3cc7266cd4365
nixonomunga <nixonomunga@gmail.com>
2023-08-12 01:23:28 -0700

    print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+/*
+ * File: 100-print_comb3.c
+ *
+ * Made by: Nixon
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ *
+ * Description: prints number combination 01 to 98
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int u = n = '0';
+
+	for (; u <= '9'; u++)
+	{
+		for (; n <= '9'; n++)
+		{
+			putchar(u);
+			putchar(n);
+
+			if ((u != '9') || (u == '9' && n != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
