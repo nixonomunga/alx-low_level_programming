653a168cdb40c3d3319c68807dbf3578c32dac2b
nixonomunga <nixonomunga@gmail.com>
2023-08-12 03:10:44 -0700

    combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,16 +22,19 @@ int main(void)
 	{
 		for (b = a + 1; b < 100; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-
-			if (a != 99 || b != 99)
+			if (a <= b)
 			{
-				putchar(',');
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
 				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+
+				if (a != 99 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
