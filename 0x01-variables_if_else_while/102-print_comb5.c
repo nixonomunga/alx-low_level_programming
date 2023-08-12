606c8b3d99ac44718ed2b4fc1531e60bdf4feb2e
nixonomunga <nixonomunga@gmail.com>
2023-08-12 03:01:23 -0700

    combination 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+/*
+ * File: 102-print_comb5.c
+ *
+ * Made by: Nixon
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ *
+ * Description: print two two-digit numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = a; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a != 99 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
