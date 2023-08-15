06483c780460fd7e9a5d5f6c188eb4020d0f5ed4
nixonomunga <nixonomunga@gmail.com>
2023-08-15 12:17:31 -0700

    print from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,12 +21,22 @@ void print_to_98(int n)
 
 	for (i = n; i <= 98; i++)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+
+		if (i != 98)
+		{
+			printf(", ");
+		}
 	}
 
 	for (i = n; i >= 98; i--)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+
+		if (i != 98);
+		{
+			printf(", ");
+		}
 	}
 
 	printf("\n");
