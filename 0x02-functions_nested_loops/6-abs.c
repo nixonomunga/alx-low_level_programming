d17238cfa9a60eaffb6372d4b2629f79e6abd5a8
nixonomunga <nixonomunga@gmail.com>
2023-08-15 05:48:17 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+/*
+ * File: 6-abs.c
+ *
+ * Made by: Nixon
+ */
+
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ *
+ * Return: integer
+ */
+
+int _abs(int)
+{
+	int a;
+
+	if (a >= 0)
+	{
+		return (a);
+	}
+
+	else
+	{
+		return (-a);
+	}
+}
