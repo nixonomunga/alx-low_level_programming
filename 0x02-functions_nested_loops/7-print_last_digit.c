809c1e944a8035f5200cf88b5a766bc33709ff1e
nixonomunga <nixonomunga@gmail.com>
2023-08-15 06:10:12 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+/*
+ * File: 7-print_last_digit.c
+ *
+ * Made by: Nixon
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of number
+ *
+ * @n: number
+ *
+ * Return: integer
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if ((n > 0) || (n < 0))
+	{
+		x = (n % 10);
+		return (x);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
