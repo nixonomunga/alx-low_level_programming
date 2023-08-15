abdba881099e92ab375cdc38971ab89334db66ab
nixonomunga <nixonomunga@gmail.com>
2023-08-15 11:43:18 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+/*
+ * File name: 10-add.c
+ *
+ * Made by: Nixon
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integer
+ *
+ * @x: first number
+ *
+ * @y: second number
+ *
+ * Return: integer if successful
+ */
+
+int add(int x, int y)
+{
+	int _addition = x + y;
+
+	return (_addition);
+}
