f967c45c5ce4c26f11b9773b7987426f768001b5
nixonomunga <nixonomunga@gmail.com>
2023-08-15 09:45:19 -0700

    Timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,27 +23,24 @@ void times_table(void)
 		{
 			int product = row * col;
 
-			if (product >= 10)
+			if (col == 10)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar(row + '0');
 			}
 
-			else if (product < 10 && row != 0)
+			if ((product < 10) && (row != 0))
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
-			}
 
-			if (col != 9)
+			else if (product >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
+				_putchar((product / 10) + '0');
+				_putchar(product % 10) + '0');
 		}
 
 		_putchar('\n');
