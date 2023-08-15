b2e2baf5eeb58d2dee56ab8f46901de483d4d00e
nixonomunga <nixonomunga@gmail.com>
2023-08-15 04:18:44 -0700

    is lower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,2 @@
-/**
- * _islower - checks for lowercase
- *
- * @c: integer
- *
- * Return: 1 when successful
- */
-
-int _islower(int c)
-{
-	if ((c >= 'a') && (c <= 'z'))
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (0);
-	}
-}
+int _putchar(char c);
+int _islower(int c);
