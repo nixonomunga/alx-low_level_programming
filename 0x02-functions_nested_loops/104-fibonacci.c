6dada8e313a97fa6b791dc7b3610100ebc1c7805
nixonomunga <nixonomunga@gmail.com>
2023-08-16 06:07:40 -0700

    first 98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+/*
+ * File: 104-fibonacci.c
+ *
+ * Made By: Nixon
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints first 98 fibonacci
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	long prev = 1;
+	long current = 2;
+	long next;
+	int limit = 98;
+
+	printf("%d, %d", prev, current);
+
+	for (i = current; i < limit; i++)
+	{
+		next = current + prev;
+
+		printf(", %lu", next);
+
+		current = prev;
+		prev = next;
+	}
+
+	return (0);
+}
