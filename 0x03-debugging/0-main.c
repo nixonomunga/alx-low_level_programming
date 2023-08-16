17eedfb27f7fef2741c32ebdf2bc4a313632f807
nixonomunga <nixonomunga@gmail.com>
2023-08-16 08:08:44 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-main.c
+ *
+ * Made by Nixon
+ */
+
+#include "main.h"
+
+/**
+ * main - tests if number is +ve or -ve
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
