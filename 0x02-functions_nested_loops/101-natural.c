70875b6f96c28e83fc3feb5941732223640f01ac
nixonomunga <nixonomunga@gmail.com>
2023-08-16 01:19:12 -0700

    Natural Numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -17,9 +17,12 @@ int main(void)
 	int x, y, _multiple;
 	int i;
 
-	_multiple = x * y;
+	if (((x % 3) == 0) && ((y % 5) == 0))
+	{
+		x = y = 0;
+		_multiple = x * y;
 
-	for (i = 0; i < 1024; i++)
+		for (i = x = y = 0; i < 1024; i++)
 	{
 		_multiple /= _multiple;
 		printf("%d\n", _multiple);
