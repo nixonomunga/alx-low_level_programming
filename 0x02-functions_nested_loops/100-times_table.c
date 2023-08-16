f5b4fc87d1ce34593f6a56e896c9ecf45514cfaa
nixonomunga <nixonomunga@gmail.com>
2023-08-16 00:56:46 -0700

    Timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,45 +16,35 @@ void print_times_table(int n)
 	{
 		return;
 	}
-
 	for (x = 0; x <= n; x++)
 	{
 		for (y = 0; y <= n; y++)
 		{
 			result = x * y;
-
 			if (result < 10)
 			{
-				_putchar(result + '0');
-				if (y < n)
+				if (y != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 				}
-			}
-			else if (result >= 10 && result < 100)
+				_putchar(result + '0');
+			} else if (result >= 10 && result < 100)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
-				if (y < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-			else if (result >= 100)
+			} else if (result >= 100)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((result / 100) + '0');
 				_putchar(((result / 10) % 10) + '0');
 				_putchar((result % 10) + '0');
-				if (y < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
