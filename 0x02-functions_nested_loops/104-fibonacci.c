adf48d247c7d5de2443be9d1d80b4efb016a830a
nixonomunga <nixonomunga@gmail.com>
2023-08-16 12:52:19 -0700

    Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,21 +15,37 @@
 int main(void)
 {
 	int i;
-	unsigned long int prev = 1;
-	unsigned long int current = 2;
-	unsigned long int next;
+	unsigned long int prev = 1, a = 0, hold_1;
+	unsigned long int current = 2, b = 0, hold_2, hold_3;
+	unsigned long int next, MOD = 1000000000;
 	int limit = 98;
 
 	printf("%lu, %lu", prev, current);
 
 	for (i = current; i < limit; i++)
 	{
-		next = current + prev;
+		if (current + prev > MOD || b > 0 || a > 0)
+		{
+			hold_1 = (current + prev) / MOD;
+			hold_2 = (current + prev) % MOD;
+			a = b, b = hold_3;
+			prev = current = hold_2;
 
-		printf(", %lu", next);
+			printf("%lu%010lu", b, current);
+		}
+		else
+		{
+			hold_2 = current + prev;
+			prev = current;
+			current = hold_2;
 
-		prev = current;
-		current = next;
+			printf("%lu", current);
+		}
+
+		if (i != 97)
+		{
+			printf(", ");
+		}
 	}
 
 	printf("\n");
