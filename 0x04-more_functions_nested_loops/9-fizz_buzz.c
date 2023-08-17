efba4d54275188f23ac8a7716aa013bd3aee8ba1
nixonomunga <nixonomunga@gmail.com>
2023-08-17 04:42:42 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints fizz for 3 multiples and fuzz for five
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+
+	for (; a <= 100; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
+		}
+	}
+}
