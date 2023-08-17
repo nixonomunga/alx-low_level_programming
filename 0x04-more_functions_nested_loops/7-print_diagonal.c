7c217e7391aae9eb52fb8a01ae30529dc41f602e
nixonomunga <nixonomunga@gmail.com>
2023-08-17 03:49:28 -0700

    Diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagnol line
+ *
+ * @n: number of times to print
+ *
+ * Return: none
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		int i = 1;
+
+		for (; i <= n; i++)
+		{
+			int a = 1;
+
+			if (a = 1)
+			{
+				_putchar('\\');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,6 +10,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 
 
 #endif
