c9c5132f279138fb5af49cd632237d7ae5877ee0
nixonomunga <nixonomunga@gmail.com>
2023-08-17 02:21:00 -0700

    Is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,16 +8,13 @@
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 'A'; i <= 'Z'; i++)
+	if (c == i)
 	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-			return (0);
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 
 	return (1);
