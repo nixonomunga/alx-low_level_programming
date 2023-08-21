ad75f637b8721361dff67dcea51e4460838d21f5
nixonomunga <nixonomunga@gmail.com>
2023-08-21 00:57:33 -0700

    Swaps integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps integers
+ *
+ * @a: first integer to be swapped
+ *
+ * @b: second integer to be swapped
+ *
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
