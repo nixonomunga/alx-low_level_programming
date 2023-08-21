7746fe12f76723d48a71ab16b306fb7a4738e23d
nixonomunga <nixonomunga@gmail.com>
2023-08-21 17:47:43 +0300

    Prints every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+/**
+ * puts2 - prints every other character of string
+ *
+ * @str: string
+ *
+ * Return: none
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
