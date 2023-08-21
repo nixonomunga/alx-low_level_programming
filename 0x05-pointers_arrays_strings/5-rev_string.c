a19d9f3a552839e2fcd72f49225dd4abfca4084b
nixonomunga <nixonomunga@gmail.com>
2023-08-21 15:55:29 +0300

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+}
