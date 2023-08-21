60866b2714a37a346d62de5af0f02d1c93a89346
nixonomunga <nixonomunga@gmail.com>
2023-08-21 03:02:11 -0700

    Prints string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts - prints a string
  *
@@ -11,7 +13,7 @@ void _puts(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-		;
+	{
+		return (str);
 
-	return (str);
 }
