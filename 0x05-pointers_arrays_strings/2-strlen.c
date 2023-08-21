ecf843bd732b2cbc4a631cb1ff494794a6d597e4
nixonomunga <nixonomunga@gmail.com>
2023-08-21 12:11:33 +0300

    String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: string to be returned
+ *
+ * @Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i > 0)
+	{
+		if (i != '\0')
+		{
+			return (i);
+		}
+	}
+}
