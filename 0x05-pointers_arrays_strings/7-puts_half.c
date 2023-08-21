6a4538878756d239486d4b1385ff3a761ea409f7
nixonomunga <nixonomunga@gmail.com>
2023-08-21 18:05:22 +0300

    Prints string in half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of string
+ *
+ * @str: string to be printed
+ *
+ * Return: none
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != 0; i++)
+	{
+		if (i % 2 != 0)
+		{
+			_putchar(str[(i - 1) / 2]);
+		}
+		else
+		{
+			_putchar(str[i / 2]);
+		}
+		_putchar('\n');
+	}
+}
