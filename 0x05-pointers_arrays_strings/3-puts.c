f77cf4a884c552618ce2b6ec0b343dbd5ea7c915
nixonomunga <nixonomunga@gmail.com>
2023-08-21 02:58:03 -0700

    Prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be ptinted
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (str);
+}
