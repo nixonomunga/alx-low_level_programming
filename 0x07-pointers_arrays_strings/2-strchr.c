61d2069a87c5e38d9379affa38cc31ec1dd79fb8
nixonomunga <nixonomunga@gmail.com>
2023-08-28 14:08:30 +0300

    Character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - functions that locates a charcater in a string
+ *
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: to first occurence of character c
+ */
+
+char *_strchr(char *s, char c)
+{
+       	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
