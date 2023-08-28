71cf5aebde7f0f89c4116bc131f09fbd576c8d38
nixonomunga <nixonomunga@gmail.com>
2023-08-28 16:59:41 +0300

    Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: pointer to hold characters
+ *
+ * Return: none
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
