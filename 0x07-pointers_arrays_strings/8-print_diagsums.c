44411d3d1921a3932d2f113d93928555c9edd9a4
nixonomunga <nixonomunga@gmail.com>
2023-08-28 17:49:08 +0300

    Sum of diagnols

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints diagnol of square matrix
+ *
+ * @a: pointer to matrix
+ * @size: size of matrix
+ *
+ * Return: none
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int main_diagnol = 0;
+	int sec_diagnol = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		main_diagnol += a[i * size + i];
+		sec_diagnol += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", main_diagnol, sec_diagnol);
+}
