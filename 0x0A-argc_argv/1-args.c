bee109dce608986a90e9b0675a045c2fe2ecc2ca
nixonomunga <nixonomunga@gmail.com>
2023-09-01 20:28:10 +0300

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ *
+ * @argc: no. of arguments
+ *
+ * @argv: string of the commands passed
+ *
+ * Return: 0 if successful
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+}
