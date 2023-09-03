04cb1992f07fd7e9bb075c083fb79a41f1dfbbea
nixonomunga <nixonomunga@gmail.com>
2023-09-03 19:55:42 +0300

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed to it
+ *
+ * @argc: no. of arguments
+ *
+ * @argv: string of the commands passed
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc attribute((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
