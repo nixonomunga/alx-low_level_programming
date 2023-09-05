c70b9f85fb7db7fc1465c89d3fcdf908a959df64
nixonomunga <nixonomunga@gmail.com>
2023-09-05 15:25:54 +0300

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all arguments
+ *
+ * @ac: argument count
+ * @av: pointer to array of strings
+ *
+ * Return: Pointer to new string || NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	int i, total_length = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
+			total_length += strlen(av[i]) + 1;
+		}
+	}
+
+	char *result = (char *)malloc(totaal_length + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	int position = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
+			strcpy(result + position, av[i]);
+			position += strlen(av[i]);
+			result[position] = '\n';
+			position++;
+		}
+	}
+	result[total_length] = '\0';
+
+	return (result);
+}
