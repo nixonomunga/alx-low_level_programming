1896ade961ff546dc18c661911a7abd43d4d652f
nixonomunga <nixonomunga@gmail.com>
2023-09-05 17:02:31 +0300

    STRTOW

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,33 +11,43 @@
 
 char **strtow(char *str)
 {
-	char *words = NULL;
-	int i = 0, j = 0, n;
+	int i, num_words = 0, word_index = 0, word_start = -1, word_length;
+	char *word_array;
 
-	if (strncmp(str, "", 1) || str == NULL)
-	{
-		return (NULL);
-	}
-
-	words = malloc((i + j + 1) * sizeof(char));
-
-	if (words == NULL)
-	{
-		return (NULL);
-	}
-
-	for (n = 0; n < i; n++)
-	{
-		words[n] = str[n];
-	}
-
-	i = n;
-	for (n = 0; n < j; n++)
-	{
-		words[i] = str[n];
-		i++;
-	}
-
-	words[i] = '\0';
+	if (str == NULL || strcmp(str, "") == 0)
 	return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && (i == 0 || str[i - 1] != ' '))
+			num_words++;
+	}
+
+	word_array = (char *)malloc((num_words + 1) * sizeof(char *));
+	if (word_array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && word_start == -1)
+			word_start = i;
+
+		else if ((str[i] == ' ' || str[i] == '\0') && word_start != -1)
+		{
+			word_length = i - word_start;
+			word_array[word_index] = (char *)
+				malloc((word_length + 1) * sizeof(char));
+			if (word_array[word_index] == NULL)
+				return (NULL);
+			strncpy(word_array[word_index], str + word_start,
+				word_length);
+			word_array[word_index][word_length] = '\0';
+			word_index++;
+			word_start = -1;
+		}
+	}
+	word_array[num_words] = NULL;
+	return (word_array);
 }
